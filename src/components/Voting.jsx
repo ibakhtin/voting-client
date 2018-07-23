cddb7c7b6f1ca19424de3026ba99de7b8f969d7e
Игорь Бахтин <Igor Bakhtin>
2018-07-23 21:33:35 +0300

    added and updated some files

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+
 import Winner from './Winner';
 import Vote from './Vote';
 
 export default React.createClass({
+  mixins: [PureRenderMixin],
+
   render: function() {
     return (
       <dir>
         {this.props.winner 
-            ? <Winner ref="winner" winner={this.props.winner} /> 
-            : <Vote {...this.props} />}
+          ? <Winner ref="winner" winner={this.props.winner} /> 
+          : <Vote {...this.props} />}
       </dir>
     )
   }
